b191dbb38415223ec63cb540abb347ffabc58c04
Thanh1912 <htp1912@gmail.com>
2017-06-15 16:57:02 +0700

    r

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express= require('express');
 var app = express();
-app.use(express.static(__direname+'/src/app'));
+app.use(express.static(__dirname+'/src/app'));
+
+
 app.listen(process.env.PORT||3000);
