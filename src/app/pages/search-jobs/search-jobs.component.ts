a83f76c323007c6e001c7eca390705ce3ddba3ac
Thanh1912 <htp1912@gmail.com>
2017-06-15 16:13:45 +0700

    sumibt

---
+++ src/app/pages/search-jobs/search-jobs.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-jobs',
+  templateUrl: './search-jobs.component.html',
+
+      styleUrls:['../../../assets/css/bootstrap.css','./search-jobs.component.css',
+    '../../../assets/css/animate.css'
+,'../../../assets/css/style.css'
+,'../../../assets/js/plugins/fancybox/jquery.fancybox.css'
+,'../../../assets/js/plugins/rsslider/settings.css'
+,'../../../assets/js/plugins/rsslider/layers.css'
+,'../../../assets/js/plugins/rsslider/navigation.css'
+,'../../../assets/js/plugins/jquery-ui/jquery-ui.css'
+,'../../../assets/js/plugins/bootstrap-slider/bootstrap-slider.css'
+,'../../../assets/js/plugins/owl/owl.carousel.css'
+    ]
+})
+export class SearchJobsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
