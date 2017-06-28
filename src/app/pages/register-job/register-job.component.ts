822d1c61fe1117bc779e49cf07957d26a2de97fd
Thanh1912 <htp1912@gmail.com>
2017-06-28 21:14:48 +0700

    register

---
+++ src/app/pages/register-job/register-job.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-job',
+  templateUrl: './register-job.component.html',
+  styleUrls: ['../../../assets/css/bootstrap.css',
+    '../../../assets/css/animate.css'
+,'../../../assets/css/style.css'
+,'../../../assets/js/plugins/fancybox/jquery.fancybox.css'
+,'../../../assets/js/plugins/rsslider/settings.css'
+,'../../../assets/js/plugins/rsslider/layers.css'
+,'../../../assets/js/plugins/rsslider/navigation.css'
+,'../../../assets/js/plugins/jquery-ui/jquery-ui.css'
+,'../../../assets/js/plugins/bootstrap-slider/bootstrap-slider.css'
+,'../../../assets/js/plugins/owl/owl.carousel.css']
+})
+export class RegisterJobComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
