e8f8256a4a6d96bde189d981f821ac55e1212cd2
Thanh1912 <htp1912@gmail.com>
2017-06-29 22:17:34 +0700

    submit

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,6 @@
 
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { routing }       from './pages.routing';
+import { routing } from './pages.routing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Pages } from './pages.component';
 import { HomeComponent } from './home/home.component';
@@ -17,28 +17,46 @@ import { ManagerJobApplyComponent } from './manager-job-apply/manager-job-apply.
 import { Home2Component } from './home2/home2.component';
 import { LoginEmployeeComponent } from './login-employee/login-employee.component';
 import { Angular2SocialLoginModule } from "angular2-social-login";
-import { Title }     from '@angular/platform-browser';
+import { Title } from '@angular/platform-browser';
 
 import { RegisterNtdComponent } from './register-ntd/register-ntd.component';
 
 import { RegisterJobComponent } from './register-job/register-job.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
-
+import { CandidateService } from '../services/candidate.service';
+import { FileSelectDirective } from 'ng2-file-upload';
 let providers = {
-    "google": {
-      "clientId": "1075668812758-334p3amm6s3h1nsnhlhd0bl0o1qd70t5.apps.googleusercontent.com"    //meanstack
-    },
-    "facebook": {
-      "clientId": "238814889927196",
-      "apiVersion": "v2.8" //like v2.4    //Spring mvc api
-    }
-  };
+  "google": {
+    "clientId": "1075668812758-334p3amm6s3h1nsnhlhd0bl0o1qd70t5.apps.googleusercontent.com"    //meanstack
+  },
+  "facebook": {
+    "clientId": "238814889927196",
+    "apiVersion": "v2.8" //like v2.4    //Spring mvc api
+  }
+};
 @NgModule({
-  imports: [CommonModule,routing,  ReactiveFormsModule , FormsModule,Angular2SocialLoginModule],
-  declarations: [Pages, HomeComponent, LoginComponent, RegisterComponent, HeaderComponent, FooterComponent, SearchJobsComponent, DetailJobComponent, SearchCompanyComponent, DetailCompanyComponent, ManagerAccountComponent, ManagerJobApplyComponent, Home2Component, LoginEmployeeComponent, RegisterNtdComponent, RegisterJobComponent]
-  ,providers: [ ],
-   schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [CommonModule, routing, ReactiveFormsModule, FormsModule, Angular2SocialLoginModule],
+  declarations: [Pages,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchJobsComponent,
+    DetailJobComponent,
+    SearchCompanyComponent,
+    DetailCompanyComponent,
+    ManagerAccountComponent,
+    ManagerJobApplyComponent,
+    Home2Component,
+    LoginEmployeeComponent,
+    RegisterNtdComponent,
+    RegisterJobComponent,
+    FileSelectDirective
+  ]
+  , providers: [CandidateService],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class PagesModule {
 }
