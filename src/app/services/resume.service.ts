bc39fbd903958cc98717ae7eb61a1565d8046607
Thanh1912 <htp1912@gmail.com>
2017-06-30 22:49:24 +0700

    submit

---
+++ src/app/services/resume.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';  
+@Injectable()
+export class ResumeService {
+
+  public Rest_Url: String = 'http://localhost:3000';
+  private headers = new Headers({ 'Content-Type': 'application/json', 'charset': 'UTF-8' });
+  private options = new RequestOptions({ headers: this.headers });
+
+constructor(private http: Http) { }
+  getPDFById(id): Observable<any> {
+    return this.http.get(this.Rest_Url+`/api/readPdf/resume/`+id, this.options);
+  }
+   save(items): Observable<any> {
+    return this.http.post(this.Rest_Url + '/api/resume', JSON.stringify(items), this.options);
+  }
+  
+}
---
+++ src/app/services/slider.service.ts
@@ -0,0 +1,32 @@
+
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class workplaceService {
+  public Rest_Url: String = 'http://localhost:3000';
+  private headers = new Headers({ 'Content-Type': 'application/json', 'charset': 'UTF-8' });
+  private options = new RequestOptions({ headers: this.headers });
+  constructor(private http: Http) { }
+  getall(): Observable<any> {
+    return this.http.get(this.Rest_Url + '/api/workplace').map(res => res.json());
+  }
+  count(): Observable<any> {
+    return this.http.get(this.Rest_Url + '/api/workplace/count').map(res => res.json());
+  }
+  add(items): Observable<any> {
+    return this.http.post(this.Rest_Url + '/api/workplace', JSON.stringify(items), this.options);
+  }
+  get(items): Observable<any> {
+    return this.http.get(this.Rest_Url + `/api/workplace/${items._id}`, this.options);
+  }
+  edit(items): Observable<any> {
+    return this.http.put(this.Rest_Url + `/api/workplace/${items._id}`, JSON.stringify(items), this.options);
+  }
+  delete(items): Observable<any> {
+    return this.http.delete(this.Rest_Url + `/api/workplace/${items._id}`, this.options);
+  }
+}
+
