d4811c9cdc39c77da75c26a6ab15ec7178672dc4
truonglam <cntt.clc@gmail.com>
2017-07-01 08:51:33 +0700

    insert table

---
+++ applicationfiles/database/05_add_column_resume.sql
@@ -0,0 +1,9 @@
+ALTER TABLE resume ADD recruiterid BIGINT;
+
+ALTER TABLE resume ADD status INT;
+-- 1: approve
+-- 2: waiting interview
+-- 3: pass
+-- 4: fail
+ALTER TABLE resume ADD createddate TIMESTAMP;
+ALTER TABLE resume ADD modifieddate TIMESTAMP;
