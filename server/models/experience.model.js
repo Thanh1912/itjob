6ebe3081010418c8b889b274d3ae3de29ced693e
Thanh1912 <htp1912@gmail.com>
2017-07-01 19:29:33 +0700

    edit

---
+++ server/models/experience.model.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose'),
+  Schema                  = mongoose.Schema;
+ mongooseUniqueValidator = require('mongoose-unique-validator');
+const districtSchema = new mongoose.Schema({
+  name: String,
+ 
+});
+
+const model = mongoose.model('experience.', districtSchema);
+
+module.exports = model;
