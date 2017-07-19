fe5cab7571ca7f3bd85f1f76c32392dd1d298dd2
Thanh1912 <htp1912@gmail.com>
2017-07-19 23:08:25 +0700

    version13

---
+++ src/app/pages_employee/shared/toast/toast.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-toast',
+  templateUrl: './toast.component.html',
+  styleUrls: ['./toast.component.scss']
+})
+export class ToastComponent {
+  @Input() message = { body: '', type: '', position: '' };
+
+  setMessage(body, type, position, time = 4444443000) {
+    this.message.body = body;
+    this.message.type = type;
+    this.message.position = position;
+
+    setTimeout(() => { this.message.body = ''; }, time);
+  }
+  close() {
+    this.message.type = "close";
+  }
+}
+
+
+
+/*
+var audio = new Audio();
+audio.src = "http://remote.address.com/example.mp3";
+audio.load();
+audio.play();
+*/
